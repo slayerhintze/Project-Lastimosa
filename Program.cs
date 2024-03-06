eefb8a26afb8ad07accba4b1a6977fe674d5d42f
slayerhintze <bradenhintze@gmail.com>
2024-03-06 15:27:17 -0700

    Connection and single event
    I got the WebSocket server to connect, and I was able to get a single 'Init' event from the game, but I don't get any other events.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.IO;
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
-using System.Net.WebSockets;
 using System.Text;
-using System.Text.RegularExpressions;
+using Rtech.Liveapi;
 
 namespace Project_Lastimosa
 {
@@ -12,10 +9,22 @@ namespace Project_Lastimosa
     {
         static void Main(string[] args)
         {
+            Repl();
+        }
+
+        /// <summary>
+        /// This method establishes a connection to the Apex Legends LiveAPI and returns a TcpClient that can be used to recieve the 
+        /// proto buf calls.
+        /// </summary>
+        /// <param name="ipAddress">String containing IPv4 IP address</param>
+        /// <param name="port">Int containing the port to listen into</param>
+        /// <returns></returns>
+        async static public void Repl()
+        {
             // First let's do some testing to get the WebSocket server up and running
             // These two constants are values outlined in the README.md for this project, and as such should not be tampered with.
             string LIVE_API_IP_ADDRESS = "127.0.0.1";
-            int LIVE_API_PORT = 443;
+            int LIVE_API_PORT = 7777;
 
             using TcpListener liveAPIConnection = new TcpListener(IPAddress.Parse(LIVE_API_IP_ADDRESS), LIVE_API_PORT);
             liveAPIConnection.Start();
@@ -32,16 +41,20 @@ namespace Project_Lastimosa
                 while (true)
                 {
                     while (!dataStream.DataAvailable) ;
+                    while (liveAPIClient.Available < 3) ;
 
                     byte[] bytes = new byte[liveAPIClient.Available];
 
                     dataStream.Read(bytes, 0, bytes.Length);
 
                     String data = Encoding.UTF8.GetString(bytes);
+                    //Console.WriteLine($"This is something {data}");
 
                     if (new System.Text.RegularExpressions.Regex("^GET").IsMatch(data))
                     {
-                        Console.WriteLine("Another");
+                        Console.WriteLine("====Client handshake====");
+                        Console.WriteLine(data);
+                        Console.WriteLine();
 
                         const string eol = "\r\n"; // HTTP/1.1 defines the sequence CR LF as the end-of-line marker
 
@@ -51,7 +64,7 @@ namespace Project_Lastimosa
                             + "Sec-WebSocket-Accept: " + Convert.ToBase64String(
                                 System.Security.Cryptography.SHA1.Create().ComputeHash(
                                     Encoding.UTF8.GetBytes(
-                                        new System.Text.RegularExpressions.Regex("Sec-WebSocket-Key: (.*)").Match(data).Groups[1].Value.Trim()
+                                        new System.Text.RegularExpressions.Regex("Sec-WebSocket-Key: (.*)").Match(data).Groups[1].Value.Trim() + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
                                     )
                                 )
                             ) + eol
@@ -61,31 +74,69 @@ namespace Project_Lastimosa
                     }
                     else
                     {
-                        Console.WriteLine("Something");
-                        Console.WriteLine(data);
+                        Console.WriteLine("Line");
+                        bool fin = (bytes[0] & 0b10000000) != 0,
+                        mask = (bytes[1] & 0b10000000) != 0; // must be true, "All messages from the client to the server have this bit set"
+                        int opcode = bytes[0] & 0b00001111, // expecting 1 - text message
+                            offset = 2;
+                        ulong msglen = bytes[1] & (ulong)0b01111111;
+
+                        if (msglen == 126)
+                        {
+                            // bytes are reversed because websocket will print them in Big-Endian, whereas
+                            // BitConverter will want them arranged in little-endian on windows
+                            msglen = BitConverter.ToUInt16(new byte[] { bytes[3], bytes[2] }, 0);
+                            offset = 4;
+                        }
+                        else if (msglen == 127)
+                        {
+                            // To test the below code, we need to manually buffer larger messages — since the NIC's autobuffering
+                            // may be too latency-friendly for this code to run (that is, we may have only some of the bytes in this
+                            // websocket frame available through client.Available).
+                            msglen = BitConverter.ToUInt64(new byte[] { bytes[9], bytes[8], bytes[7], bytes[6], bytes[5], bytes[4], bytes[3], bytes[2] }, 0);
+                            offset = 10;
+                        }
+
+                        if (msglen == 0)
+                        {
+                            Console.WriteLine("msglen == 0");
+                        }
+                        else if (mask)
+                        {
+                            byte[] decoded = new byte[msglen];
+                            byte[] masks = new byte[4] { bytes[offset], bytes[offset + 1], bytes[offset + 2], bytes[offset + 3] };
+                            offset += 4;
+
+                            for (ulong i = 0; i < msglen; ++i)
+                                decoded[i] = (byte)(bytes[(ulong)offset + i] ^ masks[i % 4]);
+
+                            LiveAPIEvent msg;
+                            try
+                            {
+                                msg = LiveAPIEvent.Parser.ParseFrom(decoded);
+                                if (msg.GameMessage.Is(Init.Descriptor))
+                                {
+                                    var msgResult = msg.GameMessage.Unpack<Init>();
+                                    Console.WriteLine(msgResult.ToString());
+                                }
+                                else
+                                {
+                                    Console.WriteLine(msg.ToString());
+                                }
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine("Womp");
+
+                            }
+                        }
+                        else
+                            Console.WriteLine("mask bit not set");
+
+                        Console.WriteLine();
                     }
                 }
             }
-        }
-
-        /// <summary>
-        /// This method establishes a connection to the Apex Legends LiveAPI and returns a TcpClient that can be used to recieve the 
-        /// proto buf calls.
-        /// </summary>
-        /// <param name="ipAddress">String containing IPv4 IP address</param>
-        /// <param name="port">Int containing the port to listen into</param>
-        /// <returns></returns>
-        static public TcpClient CreateAPIConnection(string ipAddress, int port)
-        {
-            using TcpListener liveAPIConnection = new TcpListener(IPAddress.Parse(ipAddress), port);
-            liveAPIConnection.Start();
-
-            Console.WriteLine($"Opened WebSocket server on IP address: {ipAddress} and port: {port}");
-
-            using TcpClient tcpClient = liveAPIConnection.AcceptTcpClient();
-
-            Console.WriteLine("Client connected");
-            return tcpClient;
         }
     }
 }
---
+++ WebsocketServer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Project_Lastimosa
+{
+    public class WebsocketServer
+    {
+    }
+}
